1834bbf8343ddae6409ab1d541b232c25595646f
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-28 11:34:50 -0300

    code-split para renderização dos tipos

---
+++ src/components/Type/index.jsx
@@ -0,0 +1,16 @@
+import { Text, Types } from "../../styles"
+
+export const Type = ({ type1, type2 }) => {
+  return (
+    <>
+      {(!!type2 && (
+        <Types>
+          <Text type={type1}>{type1}</Text>
+          <Text type={type2}>{type2}</Text>
+        </Types>
+      )) || (
+        <Text type={type1}>{type1}</Text>
+      )}
+    </>
+  )
+}
