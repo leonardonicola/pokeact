b8a85a161416ed5868f74a45dce4d824c11a6c13
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-28 11:33:53 -0300

    adição de peso, altura, xp base e stats

--- src/components/PokeDetail/index.jsx
+++ src/components/PokeDetail/index.jsx
@@ -1,7 +1,17 @@
-import { Wrapper, Image } from "./styles"
-import { Text, Types } from "../../styles"
-import React, { useEffect, useState } from "react"
 import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { Type } from "../Type"
+import {
+  Wrapper,
+  Image,
+  WrapperInfos,
+  Info,
+  BasicInfos,
+  WrapperStats,
+  Stat,
+  SingleStat,
+} from "./styles"
+import { Text } from "../../styles"
 import { toUpperCase } from "../../utils/toUpper"
 
 export const PokeDetails = React.memo(({ pokeId }) => {
@@ -21,22 +31,57 @@ export const PokeDetails = React.memo(({ pokeId }) => {
       {poke.name && (
         <>
           <Image src={poke.sprites.front_default} />
-          <Text paragraph>Nº{poke.id}</Text>
+          <Text paragraph>#{poke.id}</Text>
           <Text heading>{toUpperCase(poke.name)}</Text>
-          {(!!poke.types[1] && (
-            <Types>
-              <Text type={poke.types[0].type.name}>
-                {poke.types[0].type.name}
-              </Text>
-              <Text type={poke.types[1].type.name}>
-                {poke.types[1].type.name}
-              </Text>
-            </Types>
-          )) || (
-            <Text type={poke.types[0].type.name}>
-              {poke.types[0].type.name}
-            </Text>
-          )}
+          <Type
+            type1={poke.types[0].type.name.toUpperCase()}
+            {...(poke.types[1]
+              ? { type2: poke.types[1].type.name.toUpperCase() }
+              : "")}
+          />
+          <Text headingsec>ABILITIES</Text>
+          <WrapperInfos>
+            <Info ability>{toUpperCase(poke.abilities[0].ability.name)}</Info>
+            {poke.abilities[1] && (
+                <Info ability>{toUpperCase(poke.abilities[1].ability.name)}</Info>
+            ) }
+          </WrapperInfos>
+          <WrapperInfos>
+            <BasicInfos>
+              <Text headingsec>Weight</Text>
+              <Info>{poke.weight}lbs</Info>
+            </BasicInfos>
+            <BasicInfos>
+              <Text headingsec>Height</Text>
+              <Info>{poke.height}m</Info>
+            </BasicInfos>
+            <BasicInfos>
+              <Text headingsec>Base EXP</Text>
+              <Info>{poke.base_experience}</Info>
+            </BasicInfos>
+          </WrapperInfos>
+          <WrapperStats>
+            <Stat>
+              <SingleStat stat="hp">HP</SingleStat>
+              <SingleStat num>{poke.stats[0].base_stat}</SingleStat>
+            </Stat>
+            <Stat>
+              <SingleStat stat="atk">ATK</SingleStat>
+              <SingleStat num>{poke.stats[1].base_stat}</SingleStat>
+            </Stat>
+            <Stat>
+              <SingleStat stat="def">DEF</SingleStat>
+              <SingleStat num>{poke.stats[2].base_stat}</SingleStat>
+            </Stat>
+            <Stat>
+              <SingleStat stat="spc">SPC</SingleStat>
+              <SingleStat num>{poke.stats[3].base_stat}</SingleStat>
+            </Stat>
+            <Stat>
+              <SingleStat stat="spd">SPD</SingleStat>
+              <SingleStat num>{poke.stats[5].base_stat}</SingleStat>
+            </Stat>
+          </WrapperStats>
         </>
       )}
     </Wrapper>
--- src/components/PokeDetail/styles.js
+++ src/components/PokeDetail/styles.js
@@ -1,16 +1,99 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 export const Wrapper = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
-  min-width: 20%;
-  max-width: 30%;
+  min-width: 25%;
+  max-width: 40%;
+  height: fit-content;
+  padding: 10px;
   background-color: #fff;
   border-radius: 20px;
-  margin: 40px 100px 40px 0;
+  margin: 40px;
+  margin-left: 0;
 `
 
 export const Image = styled.img`
+  width: 40%;
+  transform: scale(1.3);
+`
+
+export const BasicInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
+`
+
+export const WrapperInfos = styled.div`
+  display: grid;
+  width: 90%;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+  justify-items: center;
+  margin: 15px;
+`
+export const Info = styled.span`
+  ${(props) =>
+    props.ability &&
+    css`
+      border: 0.5px solid #252525cc;
+    `}
+  background-color: #eff1f7;
+  padding: 10px;
+  border-radius: 10px;
+  width: 80%;
+  text-align: center;
+`
+export const WrapperStats = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 80%;
+  padding: 30px;
+  justify-content: space-between;
+  gap: 10px;
+`
+
+export const Stat = styled.div`
+  text-align: center;
+  background-color: #eff1f7;
+  padding: 0 2px 15px 2px;
+  border-radius: 15px;
+`
+
+export const SingleStat = styled.div`
+  ${(props) =>
+    props.stat &&
+    css`
+      padding: 9px;
+      border-radius: 15px;
+      color: #fff;
+      margin-bottom: 7px;
+    `}
+  ${({ stat }) =>
+    (stat === "hp" &&
+      css`
+        background-color: #DF2140;
+      `) ||
+    (stat === "atk" &&
+      css`
+        background-color: #FD9952;
+      `)||
+    (stat === "def" &&
+      css`
+        background-color: #FEDC51;
+      `)||
+    (stat === "spc" &&
+      css`
+        background-color: #FB94A8;
+      `)
+    ||
+    (stat === "spd" &&
+      css`
+        background-color: #85DDFF;
+      `)}
+
+    font-weight: bold;
 `
