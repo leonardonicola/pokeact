ca96571a8c456e62461f3c1a4b46502d2a791298
Leonardo Nicola <leonardonicolares@gmail.com>
2022-09-29 12:37:50 -0300

    reducer estava avançando de 10 em 10 ao inves de 6 em 6

--- src/contexts/reducer.js
+++ src/contexts/reducer.js
@@ -1,10 +1,10 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case "INCREMENT_OFFSET": {
-      return { ...state, offset: state.offset + 10 }
+      return { ...state, offset: state.offset + 6 }
     }
     case "DECREMENT_OFFSET": {
-      return { ...state, offset: state.offset - 10 }
+      return { ...state, offset: state.offset - 6 }
     }
     case "CHANGE_ID": {
       return { ...state, id: action.payload }
